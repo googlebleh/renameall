3d3233a32256c83b7205793adb39ac7341396f26
Colin Wee <2gbleh@gmail.com>
2023-03-23 15:34:29 -0700

    package up example to not rely on filesystem

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 fn main() -> anyhow::Result<()>
 {
+    let input_fnames = [
+        "Aug 2017.pdf",
+        "Sep 2017.pdf",
+        "Oct 2017.pdf",
+        "Nov 2017.pdf",
+        "Dec 2017.pdf",
+        "Jun 2017.pdf",
+        "Jul 2017.pdf",
+        "Nov 2020.pdf",
+        "Dec 2020.pdf",
+        "Jan 2021.pdf",
+        "Feb 2021.pdf",
+        "Mar 2021.pdf",
+    ];
+
     let fmts = [
         "%B %d %Y.pdf",
         "%B %Y.pdf",
     ];
-    for entry in std::fs::read_dir(".")? {
-        let entry = entry?;
-        let fname = String::from(entry.file_name().to_str().unwrap());
+    for fname in &input_fnames {
 
         for orig_fmt in &fmts {
             if let Ok(date) = chrono::NaiveDate::parse_from_str(&fname, orig_fmt) {
                 let new_fname = format!("{}", date.format("%Y.%m.%d.pdf"));
                 println!("{} --> {}", fname, new_fname);
-                std::fs::rename(&fname, new_fname);
             }
         }
     }
